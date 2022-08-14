97917a973c9aa7bfc4dfeefdf64fa6229258b27d
T00KER <gottfridolsson@hotmail.com>
2022-08-14 21:55:41 +0200

    New analysis

--- PMAS_analysis.py
+++ PMAS_analysis.py
@@ -22,17 +22,17 @@ from scipy.optimize import curve_fit
 ## CONSTANTS ##
 
 CSV_DELIMITER = ','
-CSV_PATH = 'PMAS Latitude Measurements [2022-08-01].csv'
+CSV_PATH = 'PMAS Latitude Measurements [2022-08-14].csv'
 
 LATITUDE_DOKTOR_FORSELIUS_BACKE_50_GBG = 57.6786 #degree north, Google Maps
 EARTHS_AXIAL_TILT = 23.44; # degrees, https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
 
 START_DATE = "2021-01-01" #first_date: 2021-02-27;  PMAS v. beta from: 2022-01-06;  PMAS v. beta (more accurate?) from: 2022-04-18
-END_DATE   = "2022-08-01"
+END_DATE   = "2022-08-14"
 EXTRA_DAYS_PLOT = 10
 
 EXPORT_DATA_TO_CSV = True
-UGLY_PLOT = False
+UGLY_PLOT = True
 PRINT_FITTED_AND_THEORETICAL_VALUES = True
 
 
